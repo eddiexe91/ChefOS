7e57e610854c1d45d2502fa640144bf04bb6d13c
eddiexe91 <eddie.joshua91@gmail.com>
2026-06-23 19:07:19 -0400

    Update database.types.ts

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -1,5 +1,4 @@
 // PLACEHOLDER — reemplazar con:
 // npx supabase gen types typescript --project-id TU_PROJECT_ID > src/types/database.types.ts
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Database = any
